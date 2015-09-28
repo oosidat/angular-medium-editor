5dd44967a4422ce88c610e7dccf8fafc425cdc4e
Osama Sidat <osama.sidat@d2l.com>
2015-09-28 15:23:53 -0400

    Demo for basic insert plugin integration

--- bower_components/angular-medium-editor/src/angular-medium-editor.js
+++ bower_components/angular-medium-editor/src/angular-medium-editor.js
@@ -70,12 +70,13 @@ angular.module('angular-medium-editor', [])
             }
 
             this.editor = new MediumEditor(iElement, opts);
+            angular.element(iElement).mediumInsert(scope.$eval({editor: this.editor, enabled: true}));
           }
 
           iElement.html(ctrl.$isEmpty(ctrl.$viewValue) ? '' : ctrl.$viewValue);
-          
+
           // hide placeholder when view is not empty
-          if(!ctrl.$isEmpty(ctrl.$viewValue)) angular.element(iElement).removeClass('medium-editor-placeholder'); 
+          if(!ctrl.$isEmpty(ctrl.$viewValue)) angular.element(iElement).removeClass('medium-editor-placeholder');
         };
 
       }
