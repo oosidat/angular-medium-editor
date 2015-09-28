17ab61ab20b1787ee28af7de2813ba8c72d93aeb
Osama Sidat <osama.sidat@d2l.com>
2015-09-28 16:29:12 -0400

    Updated Demo Content

--- demo/demo.js
+++ demo/demo.js
@@ -3,32 +3,36 @@
 angular.module('demo', ['angular-medium-editor'])
 
 .controller('demo', ['$scope', function($scope) {
-
+  
   $scope.text =
-  '<p>My father’s family name being <a href="https://en.wikipedia.org/wiki/Pip_(Great_Expectations)"' +
-  '>Pirrip</a>, and my Christian name Philip, my infant tongue could make of both names nothing long' +
-  'er or more explicit than Pip. So, I called myself Pip, and came to be called Pip.</p>'             +
-  '<p>I give Pirrip as my father’s family name, on the authority of his tombstone and my sister,—Mrs' +
-  '. Joe Gargery, who married the blacksmith. As I never saw my father or my mother, and never saw a' +
-  'ny likeness of either of them (for their days were long before the days of photographs), my first' +
-  'fancies regarding what they were like were unreasonably derived from their tombstones. The shape ' +
-  'of the letters on my father’s, gave me an odd idea that he was a square, stout, dark man, with cu' +
-  'rly black hair. From the character and turn of the inscription, “Also Georgiana Wife of the Above' +
-  ',” I drew a childish conclusion that my mother was freckled and sickly. To five little stone loze' +
-  'nges, each about a foot and a half long, which were arranged in a neat row beside their grave, an' +
-  'd were sacred to the memory of five little brothers of mine,—who gave up trying to get a living, ' +
-  'exceedingly early in that universal struggle,—I am indebted for a belief I religiously entertaine' +
-  'd that they had all been born on their backs with their hands in their trousers-pockets, and had ' +
-  'never taken them out in this state of existence.</p>'                                              +
-  '<p>Ours was the marsh country, down by the river, within, as the river wound, twenty miles of the' +
-  ' sea. My first most vivid and broad impression of the identity of things seems to me to have been' +
-  ' gained on a memorable raw afternoon towards evening. At such a time I found out for certain that' +
-  ' this bleak place overgrown with nettles was the churchyard; and that Philip Pirrip, late of this' +
-  ' parish, and also Georgiana wife of the above, were dead and buried; and that Alexander, Bartholo' +
-  'mew, Abraham, Tobias, and Roger, infant children of the aforesaid, were also dead and buried; and' +
-  ' that the dark flat wilderness beyond the churchyard, intersected with dikes and mounds and gates' +
-  ', with scattered cattle feeding on it, was the marshes; and that the low leaden line beyond was t' +
-  'he river; and that the distant savage lair from which the wind was rushing was the sea; and that ' +
-  'the small bundle of shivers growing afraid of it all and beginning to cry, was Pip.</p>';
+  '<p>Mr. and Mrs. <a href="https://en.wikipedia.org/wiki/List_of_supporting_Harry_Potter_characters#The_Dursleys">Dursley</a>, of number four, Privet Drive, were proud to say '+
+  'that they were perfectly normal, thank you very much. They were the last '+
+  'people you\'d expect to be involved in anything strange or mysterious, '+
+  'because they just didn\'t hold with such nonsense.</p>'+
+  '<p>Mr. Dursley was the director of a firm called Grunnings, which made '+
+  'drills. He was a big, beefy man with hardly any neck, although he did '+
+  'have a very large mustache. Mrs. Dursley was thin and blonde and had '+
+  'nearly twice the usual amount of neck, which came in very useful as she '+
+  'spent so much of her time craning over garden fences, spying on the '+
+  'neighbors. The Dursleys had a small son called Dudley and in their '+
+  'opinion there was no finer boy anywhere.</p>'+
+  '<p>The Dursleys had everything they wanted, but they also had a secret, and '+
+  'their greatest fear was that somebody would discover it. They didn\'t '+
+  'think they could bear it if anyone found out about the Potters. Mrs. '+
+  'Potter was Mrs. Dursley\'s sister, but they hadn\'t met for several years; '+
+  'in fact, Mrs. Dursley pretended she didn\'t have a sister, because her '+
+  'sister and her good-for-nothing husband were as unDursleyish as it was '+
+  'possible to be. The Dursleys shuddered to think what the neighbors would '+
+  'say if the Potters arrived in the street. The Dursleys knew that the '+
+  'Potters had a small son, too, but they had never even seen him. This boy '+
+  'was another good reason for keeping the Potters away; they didn\'t want '+
+  'Dudley mixing with a child like that.</p> '+
+  '<p>When Mr. and Mrs. Dursley woke up on the dull, gray Tuesday our story '+
+  'starts, there was nothing about the cloudy sky outside to suggest that '+
+  'strange and mysterious things would soon be happening all over the '+
+  'country. Mr. Dursley hummed as he picked out his most boring tie for '+
+  'work, and Mrs. Dursley gossiped away happily as she wrestled a screaming '+
+  'Dudley into his high chair.</p> '+
+  '<p><i>None of them noticed a large, tawny owl flutter past the window.</i></p> ';
 
 }]);
